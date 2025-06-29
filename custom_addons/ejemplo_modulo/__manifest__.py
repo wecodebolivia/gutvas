d34f87621d90a30c5b167627a92461ecceb7bd9a
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2025-06-29 09:13:03 -0400

    modulos correcto y adicionales corregidos

--- custom_addons/ejemplo_modulo/__manifest__.py
+++ custom_addons/ejemplo_modulo/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'Ejemplo Módulo',
     'version': '1.0',
     'category': 'Tools',
+    "license": "AGPL-3",
     'summary': 'Módulo mínimo para prueba',
     'depends': ['base'],
     'data': [],
