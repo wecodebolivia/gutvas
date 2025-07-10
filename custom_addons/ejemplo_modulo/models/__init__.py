07d559e0fe25747e010c337a113415bc22b5157e
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2025-07-09 22:00:15 -0400

    v17 clean

--- custom_addons/ejemplo_modulo/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import ejemplo_modelo
--- custom_addons/ejemplo_modulo/models/ejemplo_modelo.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class EjemploModelo(models.Model):
-    _name = 'ejemplo.modelo'
-    _description = 'Ejemplo Modelo'
-
-    name = fields.Char(string='Nombre')
