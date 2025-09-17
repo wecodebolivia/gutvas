6b9b2f92a37163f8f1ddea75beceab6531e9894a
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2025-09-16 21:41:09 -0400

    boton de libro de compras

--- l10n_bo_purchase_book_line/models/account_move.py
+++ l10n_bo_purchase_book_line/models/account_move.py
@@ -4,4 +4,12 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_open_libro_compras(self):
-        return True
+        # Aquí puedes poner lógica más adelante
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Libro de Compras',
+            'res_model': 'account.move',
+            'view_mode': 'form',
+            'target': 'current',
+            'res_id': self.id,
+        }
