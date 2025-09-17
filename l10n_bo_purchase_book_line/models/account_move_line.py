d6b3cc588a838fc48ea722f2a5493acacccd1aef
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2025-09-16 20:58:32 -0400

    Update account_move_line.py

--- l10n_bo_purchase_book_line/models/account_move_line.py
+++ l10n_bo_purchase_book_line/models/account_move_line.py
@@ -1,17 +1,19 @@
-# l10n_bo_purchase_book_line/models/account_move_line.py
+# l10n_bo_purchase_book_line/models/account_move.py
+
 from odoo import models, fields
 
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
-    is_purchase_book_line = fields.Boolean(
-        string="Es línea de libro de compras", 
-        help="Indica si esta línea debe considerarse en el libro de compras."
-    )
-    nit = fields.Char(string="NIT Proveedor")
-    invoice_number = fields.Char(string="Número de Factura")
-    authorization_number = fields.Char(string="Número de Autorización")
-    control_code = fields.Char(string="Código de Control")
-    purchase_amount = fields.Monetary(string="Monto de la Compra")
-    fiscal_credit = fields.Monetary(string="Crédito Fiscal", help="13% del monto si aplica")
-    currency_id = fields.Many2one("res.currency", string="Moneda")
+    lc_numero_factura = fields.Char(string="Número de Factura")
+
+    def action_open_libro_compras_wizard(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Libro de Compras',
+            'res_model': 'libro.compras.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {'default_move_line_id': self.id},
+        }
