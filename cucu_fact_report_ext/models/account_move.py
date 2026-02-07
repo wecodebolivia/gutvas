461c6ea1579352f6a7d51eb3fe09ba7b775ef952
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2026-02-06 23:10:40 -0400

    Update account_move.py

--- cucu_fact_report_ext/models/account_move.py
+++ cucu_fact_report_ext/models/account_move.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _prepare_cucu_header_data(self):
-        """Override para modificar teléfono en header de CUCU"""
-        res = super()._prepare_cucu_header_data()
+        """ 
+        Intercepta el diccionario que usa el reporte A4 
+        y reemplaza el teléfono antiguo por el de la sucursal activa.
+        """
+        res = super(AccountMove, self)._prepare_cucu_header_data()
         
-        # Reemplazar teléfono con el del Branch/POS Config
-        if self.pos_config_id and self.pos_config_id.phone:
-            res['telefono'] = self.pos_config_id.phone
-        
+        # Obtenemos el teléfono del Branch vinculado al Punto de Venta (pos_id)
+        if self.pos_id and self.pos_id.branch_id and self.pos_id.branch_id.phone:
+            res['telefono'] = self.pos_id.branch_id.phone
+        elif self.pos_id and self.pos_id.phone:
+            res['telefono'] = self.pos_id.phone
+            
         return res
